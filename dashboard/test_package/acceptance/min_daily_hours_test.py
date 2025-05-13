375abe1dcd213a65d14d43b65bf51c5b22df02b3
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-05-13 10:27:26 +0200

    Add very simple tests for the daily acceptance criterion

---
+++ dashboard/test_package/acceptance/min_daily_hours_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+class MinDailyHoursTest(unittest.TestCase):
+    def test_given_a_minimum_of_3_daily_hours__when_calling_the_criterion_with_300_seconds_then_it_must_return_false(
+        self,
+    ) -> None:
+        from dashboard.ctxdashboard.acceptance.min_daily_hours import (
+            simple_min_hours_daily_acceptance_criterion,
+        )
+
+        min_hours = 3
+        criterion = simple_min_hours_daily_acceptance_criterion(min_hours)
+
+        result = criterion(None, 300)
+
+        self.assertFalse(result)
+
+    def test_given_a_minimum_of_3_daily_hours__when_calling_the_criterion_with_10800_seconds_then_it_must_return_true(
+        self,
+    ) -> None:
+        from dashboard.ctxdashboard.acceptance.min_daily_hours import (
+            simple_min_hours_daily_acceptance_criterion,
+        )
+
+        min_hours = 3
+        criterion = simple_min_hours_daily_acceptance_criterion(min_hours)
+
+        result = criterion(None, 10800)
+
+        self.assertTrue(result)
