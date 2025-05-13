3388adebcb77ef7afeb15e3fdddbf1cb0c37e652
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-05-13 10:23:57 +0200

    Ad very simple patient tests

---
+++ dashboard/test_package/model/patient_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from dashboard.ctxdashboard.model.patient import CTxPatient
+
+
+class PatientTest(unittest.TestCase):
+    def test_given_valid_parameters__when_initializing__then_create_a_patient(self) -> None:
+        patient = CTxPatient(
+            "PATIENT_ID",
+            [],
+            True,
+        )
+
+        self.assertIsInstance(patient, CTxPatient)
