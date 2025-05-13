f739c58e93f524ff839e0ae36bbfbb239ec47583
Paul Erpenstein <paul.erpenstein@gmail.com>
2025-05-13 13:47:26 +0200

    Fix broken interface

--- dashboard/test_package/model/patient_test.py
+++ dashboard/test_package/model/patient_test.py
@@ -1,6 +1,8 @@
 import unittest
 
 from ctxdashboard.model.patient import CTxPatient
+from ctxdashboard.model.dailies_columns import DailiesColumns as dc
+import pandas as pd
 
 
 class PatientTest(unittest.TestCase):
@@ -12,3 +14,20 @@ class PatientTest(unittest.TestCase):
         )
 
         self.assertIsInstance(patient, CTxPatient)
+    
+    def test_given_a_frame_with_one_row__when_initializing__then_create_a_patient(self) -> None:
+        patient = CTxPatient.from_frame(
+            "PATIENT_ID",
+            pd.DataFrame.from_dict({
+                dc.START_DT: [pd.Timestamp("2023-10-01")],
+                dc.END_DT: [pd.Timestamp("2023-10-02")],
+                dc.DAILY_DURATION_S: [3600],
+            }),
+            lambda _date, _duration: True,
+            lambda _days: True,
+        )
+
+        self.assertIsInstance(patient, CTxPatient)
+        self.assertEqual(patient.patient_id, "PATIENT_ID")
+        self.assertEqual(len(patient.patient_days), 1)
+        self.assertTrue(patient.accepted)
